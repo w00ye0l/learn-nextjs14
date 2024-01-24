370b25d0d79901b636b56198780b58daf6a83a5a
wooyeol <dnduf158@gmail.com>
2024-01-24 15:32:32 +0900

    fix: home page ui

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,17 +1,26 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-  return fetch(URL).then((response) => response.json());
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  return fetch(API_URL).then((response) => response.json());
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
